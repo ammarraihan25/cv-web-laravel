a96400d31ce8fa236f0e62745e3afe752c713bd2
ammarraihan25 <ammarraihan2005@gmail.com>
2026-05-14 19:47:02 +0700

    Fix: Putenv for manifest paths in api/index.php

--- api/index.php
+++ api/index.php
@@ -1,14 +1,15 @@
 <?php
 
-use Illuminate\Foundation\Application;
-use Illuminate\Http\Request;
+// Force manifest paths to /tmp before anything else
+putenv('LARAVEL_SERVICES_PATH=/tmp/services.php');
+putenv('LARAVEL_PACKAGES_PATH=/tmp/packages.php');
 
 require __DIR__ . '/../vendor/autoload.php';
 
-/** @var Application $app */
+/** @var \Illuminate\Foundation\Application $app */
 $app = require_once __DIR__ . '/../bootstrap/app.php';
 
 // Force storage path to /tmp for Vercel
 $app->useStoragePath('/tmp');
 
-$app->handleRequest(Request::capture());
+$app->handleRequest(\Illuminate\Http\Request::capture());
