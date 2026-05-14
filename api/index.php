bcfb54f29bfc43952a67224bea141145ee4e88b4
ammarraihan25 <ammarraihan2005@gmail.com>
2026-05-14 19:18:02 +0700

    Refactor api/index.php for Vercel compatibility

--- api/index.php
+++ api/index.php
@@ -1,6 +1,15 @@
 <?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+// Debugging
 ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/../public/index.php';
+require __DIR__ . '/../vendor/autoload.php';
+
+/** @var Application $app */
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$app->handleRequest(Request::capture());
