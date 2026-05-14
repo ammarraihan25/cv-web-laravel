c34c6da1c24111bcb687013a21a9f91dadec41c3
ammarraihan25 <ammarraihan2005@gmail.com>
2026-05-14 20:28:04 +0700

    Chore: Clean up debug routes

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,3 @@ Route::get('/cv', function () {
 use App\Http\Controllers\EducationHistoryController;
 
 Route::get('/education', [EducationHistoryController::class, 'index']);
-
-Route::get('/debug-assets', function () {
-    $path = public_path('build/manifest.json');
-    return [
-        'public_path' => public_path(),
-        'manifest_exists' => file_exists($path),
-        'manifest_path' => $path,
-        'base_path' => base_path(),
-        'build_dir_exists' => is_dir(public_path('build')),
-        'files_in_build' => is_dir(public_path('build')) ? scandir(public_path('build')) : 'no build dir',
-    ];
-});
